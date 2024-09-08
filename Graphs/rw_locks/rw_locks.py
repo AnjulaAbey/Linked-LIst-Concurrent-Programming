774ebc6738fdb81aede101951e75443a8622096b
AnjulaAbey <abeyweeraanjula@gmail.com>
2024-09-08 11:07:18 +0530

    graph python

---
+++ Graphs/rw_locks/rw_locks.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the CSV file
+df = pd.read_csv('rw_locks_results.csv', header=None, names=['Threads', 'Avg Execution Time', 'Standard Deviation'])
+
+# Plot the average execution time vs. number of threads
+plt.figure(figsize=(10, 6))
+plt.errorbar(df['Threads'], df['Avg Execution Time'], yerr=df['Standard Deviation'], fmt='-o', capsize=5, label='Avg Execution Time')
+
+# Add labels and title
+plt.xlabel('Number of Threads')
+plt.ylabel('Average Execution Time (seconds)')
+plt.title('Average Execution Time vs Number of Threads')
+plt.grid(True)
+plt.legend()
+
+# Show the plot
+plt.show()
